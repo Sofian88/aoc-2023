a380563a24ebea150a1288c395bf6e7baf2e25d8
Sofian <aitsaid.sofian@gmail.com>
2023-12-01 09:18:51 +0100

    2023 - Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,45 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.sumOf { line ->
+            Integer.valueOf(String(charArrayOf(line.first { it.isDigit() }, line.last { it.isDigit() })))
+        }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.sumOf { line ->
+            Integer.valueOf(String(charArrayOf(line.firstNumericValue(), line.lastNumericValue())))
+        }
     }
 
-    // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 142)
+    val testInput2 = readInput("Day01_test2")
+    check(part2(testInput2) == 281)
 
     val input = readInput("Day01")
     part1(input).println()
     part2(input).println()
 }
+
+fun String.firstNumericValue(): Char {
+    var position = this.indexOfFirst { it.isDigit() }.takeIf { it != -1 } ?: Int.MAX_VALUE
+
+    this.findAnyOf(numbers.keys, 0)?.let {
+        position = kotlin.math.min(position, it.first)
+    }
+
+    return if (this[position].isDigit()) this[position]
+    else (numbers[this.findAnyOf(numbers.keys, 0)?.second] ?: 0).toString().first()
+}
+
+
+fun String.lastNumericValue(): Char {
+    var position = this.indexOfLast { it.isDigit() }.takeIf { it != -1 } ?: 0
+
+    this.findLastAnyOf(numbers.keys, this.length - 1)?.let {
+        position = kotlin.math.max(position, it.first)
+    }
+    return if (this[position].isDigit()) this[position]
+    else (numbers[this.findLastAnyOf(numbers.keys, position)?.second] ?: 0).toString().first()
+}
+
--- src/Utils.kt
+++ src/Utils.kt
@@ -6,6 +6,20 @@ import kotlin.io.path.readLines
 /**
  * Reads lines from the given input txt file.
  */
+
+val numbers =
+    mapOf(
+        "one" to 1,
+        "two" to 2,
+        "three" to 3,
+        "four" to 4,
+        "five" to 5,
+        "six" to 6,
+        "seven" to 7,
+        "eight" to 8,
+        "nine" to 9
+    )
+
 fun readInput(name: String) = Path("src/$name.txt").readLines()
 
 /**
