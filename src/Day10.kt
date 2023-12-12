b01a23e3727a3472c4c81506aa17f2fb3cfef57c
Sofian <aitsaid.sofian@gmail.com>
2023-12-12 06:21:33 +0100

    2023 - Day 10 intermediate soluiton

--- src/Day10.kt
+++ src/Day10.kt
@@ -1,24 +1,22 @@
-import kotlin.math.max
-import kotlin.math.min
 
 fun main() {
     fun part1(input: List<String>): Long {
         var startPosition: Pair<Int, Int> = Pair(0, 0)
         val board =
             input.mapIndexed { y, row ->
-                row.split("").mapIndexed { x, c ->
+                row.mapIndexed { x, c ->
                     when (c) {
-                        "S" -> {
-                            startPosition = x - 1 to y
+                        'S' -> {
+                            startPosition = x to y
                             Animal()
                         }
 
-                        "|" -> VerticalPipe()
-                        "-" -> HorizontalPipe()
-                        "L" -> Bend1()
-                        "J" -> Bend2()
-                        "7" -> Bend3()
-                        "F" -> Bend4()
+                        '|' -> VerticalPipe()
+                        '-' -> HorizontalPipe()
+                        'L' -> Bend1()
+                        'J' -> Bend2()
+                        '7' -> Bend3()
+                        'F' -> Bend4()
                         else -> Floor()
                     }
                 }
@@ -43,62 +41,70 @@ fun main() {
 }
 
 fun findLoopDistance(board: List<List<Tile>>, startPosition: Pair<Int, Int>): Long {
-    return findLoop(board, startPosition, 0L)
+    return findLoop(board, emptySet(), startPosition, 0L)
 }
 
-private fun Array<Pair<Pair<Int, Int>, Tile>>.validate(): Array<Pair<Pair<Int, Int>, Tile>> {
-    val newArray = arrayListOf<Pair<Pair<Int, Int>, Tile>>()
-    if (this[0].second is VerticalPipe || this[0].second is Bend3 || this[0].second is Bend4) {
+private fun Array<Pair<Pair<Int, Int>, Tile?>>.validate(current: Tile): Array<Pair<Pair<Int, Int>, Tile?>> {
+    val newArray = arrayListOf<Pair<Pair<Int, Int>, Tile?>>()
+    if (this.isNotEmpty() && (current.north && this[0].second?.south == true)) {
         newArray.add(this[0])
-    } else if (this[1].second is VerticalPipe || this[1].second is Bend1 || this[1].second is Bend2) {
+    }
+    if (this.size >= 2 && (current.south && this[1].second?.north == true)) {
         newArray.add(this[1])
-    } else if (this[2].second is HorizontalPipe || this[2].second is Bend1 || this[2].second is Bend4) {
+    }
+    if (this.size >= 3 && (current.west && this[2].second?.east == true)) {
         newArray.add(this[2])
-    } else if (this[3].second is HorizontalPipe || this[3].second is Bend2 || this[3].second is Bend3) {
+    }
+    if (this.size == 4 && (current.east && this[3].second?.west == true)) {
         newArray.add(this[3])
     }
-    return newArray.toTypedArray()
+    return newArray.filterNot { it.second == null }.toTypedArray()
 }
+
 
 private fun getNeighbours(
     x: Int,
     y: Int,
     board: List<List<Tile>>
-) = arrayOf(
-    x to max(0, y - 1) to board[x][max(0, y - 1)],
-    x to min(board.size, y + 1) to board[x][min(board.size, y + 1)],
-    max(0, x - 1) to y to board[max(0, x - 1)][y],
-    min(board[x].size, x + 1) to y to board[min(board[x].size, x + 1)][y]
-)
+) : Array<Pair<Pair<Int, Int>, Tile?>> = arrayOf(
+    x to y - 1 to board.getOrNull(y - 1)?.getOrNull(x),
+    x to y + 1 to board.getOrNull(y + 1)?.getOrNull(x),
+    x - 1 to y to board[y].getOrNull(x - 1),
+    x + 1 to y to board[y].getOrNull(x + 1)
+).filterNot { it.second == null }.toTypedArray()
 
 tailrec fun findLoop(
     board: List<List<Tile>>,
-    first: Pair<Int, Int>,
+    visited: Set<Pair<Int, Int>>,
+    actualItem: Pair<Int, Int>,
     distance: Long
 ): Long {
-    val nextNeighbours = getNeighbours(first.first, first.second, board).validate()
+    val current = board[actualItem.first][actualItem.second]
+    val newVisited = visited.toMutableSet().also { it.add(actualItem) }
+    "Visited ${actualItem}".println()
+    val nextNeighbours = getNeighbours(actualItem.first, actualItem.second, board).validate(current)
     return if (nextNeighbours.any { it.second is Animal }) distance + 1
-    else if (nextNeighbours.isEmpty()) distance
+    else if (nextNeighbours.isEmpty() && current is Animal) distance
     else {
-        nextNeighbours.maxOf { findLoop(board, it.first, distance + 1) }
+        val neighbourToVisit =
+            nextNeighbours.filterNot { nextNeighbour -> newVisited.any { it == nextNeighbour.first } }.firstOrNull()
+        if (neighbourToVisit == null) {
+            "No more item to visit"
+            if(distance == 0L) return 0L
+            return findLoop(board, newVisited, newVisited.toList()[newVisited.size - 1], distance - 1)
+        } else findLoop(board, newVisited, neighbourToVisit.first, distance + 1)
     }
 }
 
 
-sealed interface Tile
-sealed interface North : Tile
-sealed interface South : Tile
-sealed interface East : Tile
-sealed interface West : Tile
-
-
-class Floor : Tile
-class Animal : Tile
-class VerticalPipe : North, South
-class HorizontalPipe : East, West
-class Bend1 : North, East
-class Bend2 : North, West
-class Bend3 : South, West
-class Bend4 : South, East
+sealed class Tile(val west: Boolean, val east: Boolean, val south: Boolean, val north: Boolean)
+class Floor : Tile(false, false, false, false)
+class Animal : Tile(true, true, true, true)
+class VerticalPipe : Tile(false, false, true, true)
+class HorizontalPipe : Tile(true, true, false, false)
+class Bend1 : Tile(false, true, false, true)
+class Bend2 : Tile(true, false, false, true)
+class Bend3 : Tile(true, false, true, false)
+class Bend4 : Tile(false, true, true, false)
 
 
